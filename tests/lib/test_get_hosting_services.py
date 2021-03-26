083ee50fc3dd887c89ab17f48cc5b61196df4a81
jan <jan@kwoh.de>
2021-03-26 18:58:43 +0100

    quick get_hosting_instances test

--- tests/lib/test_get_hosting_services.py
+++ tests/lib/test_get_hosting_services.py
@@ -1,14 +1,11 @@
 
 from hubgrep.lib.get_hosting_service_interfaces import get_hosting_service_interfaces
-from hubgrep.models import HostingService
-
-
 
 class TestGetHostingServices():
-
 
     def test_get_hosting_services(self, test_app):
         with test_app.app_context():
             hosting_services = get_hosting_service_interfaces(cache=False)
-            print(hosting_services)
+            test_hoster = list(hosting_services.values())[0]
+            assert test_hoster.name == 'Gitea'
 
