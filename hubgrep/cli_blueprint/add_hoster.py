0a2ef17104ecfd9bd4b793a09543d2f07ad6c1c2
Henrik Eliasson <he@datenfreunde.com>
2021-03-28 13:28:34 +0200

    form html/css & add hosting_service model "label"

--- hubgrep/cli_blueprint/add_hoster.py
+++ hubgrep/cli_blueprint/add_hoster.py
@@ -4,7 +4,7 @@ import json
 from hubgrep.cli_blueprint import cli_bp
 
 from hubgrep import db, security
-from hubgrep.models import HostingService
+from hubgrep.models import HostingService, get_service_label_from_url
 
 @cli_bp.cli.command()
 @click.argument("type", type=click.STRING)
@@ -25,6 +25,7 @@ def add_hoster(type, api_url, landingpage_url, config):
     # todo: validate
     h.api_url = api_url
     h.landingpage_url = landingpage_url
+    h.label = get_service_label_from_url(landingpage_url)
     h.config = config
 
     print(f"adding {h.api_url}")
--- hubgrep/frontend_blueprint/routes/index.py
+++ hubgrep/frontend_blueprint/routes/index.py
@@ -1,4 +1,3 @@
-from typing import Union
 from collections import namedtuple
 from flask import render_template
 from flask import current_app as app
@@ -10,19 +9,21 @@ from hubgrep.lib.fetch_results import fetch_concurrently
 from hubgrep.lib.filter_results import filter_results
 from hubgrep.lib.get_hosting_service_interfaces import get_hosting_service_interfaces
 from hubgrep.models import HostingService
-
 from hubgrep.frontend_blueprint import frontend
 
 checkbox = namedtuple("checkbox", "id label is_checked")
 search_form = namedtuple("form", "search_phrase services allow_forks allow_archived")
 
 
-def _get_search_form(search_phrase: Union[str, bool], allow_forks: bool, allow_archived: bool) -> search_form:
+def _get_search_form() -> search_form:
+    # we default to "on" for missing params only when search_phrase is empty (as its the landing page)
+    search_phrase = request.args.get("s", "")
+    allow_forks = search_phrase is "" or request.args.get("f", False) == "on"
+    allow_archived = search_phrase is "" or request.args.get("a", False) == "on"
     service_checkboxes = []
     for service in HostingService.query.all():
-        is_checked = search_phrase is False or request.args.get("s{}".format(service.id), False) == "on"
-        service_checkboxes.append(checkbox(id="s{}".format(service.id), label="{} - {}".format(service.landingpage_url, service.type),
-                                           is_checked=is_checked))  # TODO add label to service name instead of landingpage_url
+        is_checked = search_phrase is "" or request.args.get("s{}".format(service.id), False) == "on"
+        service_checkboxes.append(checkbox(id="s{}".format(service.id), label=service.label, is_checked=is_checked))
 
     return search_form(search_phrase=search_phrase, services=service_checkboxes,
                        allow_forks=allow_forks, allow_archived=allow_archived)
@@ -34,8 +35,6 @@ def index():
     results_offset = int(request.args.get(PARAM_OFFSET, 0))
     results_per_page = int(request.args.get(PARAM_PER_PAGE, app.config['PAGINATION_PER_PAGE_DEFAULT']))
     search_phrase = request.args.get("s", False)
-    allow_forks = search_phrase is False or request.args.get("f", False) == "on"
-    allow_archived = search_phrase is False or request.args.get("a", False) == "on"
     search_feedback = ""
     external_errors = []
     pagination_links = []
@@ -43,7 +42,7 @@ def index():
         terms = search_phrase.split()
         search_interfaces = get_hosting_service_interfaces(cache=app.config['ENABLE_CACHE'])
         results, external_errors = fetch_concurrently(terms, search_interfaces)
-        results = filter_results(results, )
+        results = filter_results(results)
         results_paginated = results[results_offset:(results_offset + results_per_page)]
         pagination_links = get_page_links(request.full_path, results_offset, results_per_page, len(results))
         search_feedback = "page {} of {} total matching repositories.".format(
@@ -51,10 +50,9 @@ def index():
 
     return render_template("search/search.html",
                            title=SITE_TITLE,
-                           search_results=results_paginated,
+                           form=_get_search_form(),
                            search_url=request.url,
-                           search_phrase=search_phrase,
+                           search_results=results_paginated,
                            search_feedback=search_feedback,
-                           form=_get_search_form(search_phrase, allow_forks, allow_archived),
                            pagination_links=pagination_links,  # [PageLink] namedtuples
                            external_errors=external_errors)  # TODO these errors should be formatted to text that is useful for a enduser
