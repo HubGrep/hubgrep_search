aa1652ab5420c9c36a58b641ed1be5c1299e746b
Henrik Eliasson <he@datenfreunde.com>
2021-04-05 21:28:22 +0200

    remove imprint page/style/route

--- hubgrep/frontend_blueprint/__init__.py
+++ hubgrep/frontend_blueprint/__init__.py
@@ -6,7 +6,6 @@ frontend = Blueprint("frontend", __name__, template_folder="templates")
 
 from hubgrep.frontend_blueprint.routes.index import index
 from hubgrep.frontend_blueprint.routes.about import about
-from hubgrep.frontend_blueprint.routes.imprint import imprint
 from hubgrep.frontend_blueprint.routes.manage import manage_instance, manage_instances
 from hubgrep.frontend_blueprint.routes.add_instance import add_instance_step_1, add_instance_step_2
 
