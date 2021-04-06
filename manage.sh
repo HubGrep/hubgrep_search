74552e4bf03e24761019d416bfa1334a1ab0af5b
Henrik Eliasson <he@datenfreunde.com>
2021-04-06 15:16:30 +0200

    revert dev test script

--- manage.sh
+++ manage.sh
@@ -48,7 +48,7 @@ else
     echo "building... TODO =("
   elif [ "$1" == "test" ]; then
     echo "running tests..."
-    pytest --cov=hubgrep . -s
+    pytest --cov=hubgrep .
   elif [ "$1" == "bumpversion" ]; then
     shift
     bumpversion $@
