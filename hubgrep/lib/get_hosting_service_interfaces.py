e2b2a809ec87c765f8b73c0ee3565d357f77d86c
jan <jan@kwoh.de>
2021-04-18 15:03:06 +0200

    separate get_cache_backend function, fix .env.dist

--- hubgrep/lib/get_hosting_service_interfaces.py
+++ hubgrep/lib/get_hosting_service_interfaces.py
@@ -18,6 +18,27 @@ hosting_service_interfaces_by_name = dict(
     github=GitHubSearch, gitlab=GitLabSearch, gitea=GiteaSearch
 )
 
+class UnknownBackendException(Exception):
+    pass
+
+def _get_cache_backend():
+    cache_backend_str = app.config['CACHE_BACKEND']
+    if cache_backend_str == None or cache_backend_str.lower() == 'none':
+        cache_backend = None
+
+    elif cache_backend_str == 'redis':
+        from requests_cache.backends import RedisCache
+
+        redis_url = app.config['REDIS_URL']
+        redis_client = redis.from_url(redis_url)
+        cache_backend = RedisCache(connection=redis_client)
+
+    elif cache_backend_str == 'memory':
+        cache_backend = 'memory'
+
+    else:
+        raise UnknownBackendException(f'unknown cache backend "{cache_backend_str}"!')
+    return cache_backend
 
 def get_hosting_service_interfaces():
     hosting_service_interfaces = {}
@@ -29,20 +50,7 @@ def get_hosting_service_interfaces():
         config = json.loads(config_str)
 
         SearchClass = hosting_service_interfaces_by_name[service.type]
-        cache_backend_str = app.config['CACHE_BACKEND']
-        if cache_backend_str == None:
-            cache_backend = None
-        elif cache_backend_str == 'redis':
-            from requests_cache.backends import RedisCache
-
-            redis_url = app.config['REDIS_URL']
-            redis_client = redis.from_url(redis_url)
-            cache_backend = RedisCache(connection=redis_client)
-        elif cache_backend_str == 'memory':
-            cache_backend = 'memory'
-        else:
-            raise f'unknown cache backend {cache_backend_str}!'
-
+        cache_backend = _get_cache_backend()
         if cache_backend:
             logger.debug(f'using cache cache backend: {cache_backend}')
             session = CachedSession(
@@ -57,6 +65,6 @@ def get_hosting_service_interfaces():
             **config,
             requests_session=session,
         )
-       
+
         hosting_service_interfaces[service.api_url] = SearchClass(**args)
     return hosting_service_interfaces
--- tests/lib/test_get_hosting_services.py
+++ tests/lib/test_get_hosting_services.py
@@ -1,10 +1,30 @@
-
-from hubgrep.lib.get_hosting_service_interfaces import get_hosting_service_interfaces
+import pytest
+from hubgrep.lib.get_hosting_service_interfaces import get_hosting_service_interfaces, _get_cache_backend, UnknownBackendException
 
 class TestGetHostingServices():
 
+    def test__get_cache_backend(self, test_app):
+        with test_app.app_context():
+            test_app.config['CACHE_BACKEND'] = 'NonE'
+            backend = _get_cache_backend()
+            assert backend is None
+
+            test_app.config['CACHE_BACKEND'] = 'memory'
+            backend = _get_cache_backend()
+            assert backend == 'memory'
+
+            test_app.config['CACHE_BACKEND'] = None
+            backend = _get_cache_backend()
+            assert backend is None
+
+            test_app.config['CACHE_BACKEND'] = 'something undefined'
+            with pytest.raises(UnknownBackendException):
+                backend = _get_cache_backend()
+
+
     def test_get_hosting_services(self, test_app):
         with test_app.app_context():
+            test_app.config['CACHE_BACKEND'] = 'memory'
             hosting_services = get_hosting_service_interfaces()
             test_hoster = list(hosting_services.values())[0]
             assert test_hoster.name == 'Gitea'
