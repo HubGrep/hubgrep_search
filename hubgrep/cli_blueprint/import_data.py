ffcc6b040ec4f0a43ec910376701952c2af8b474
jan <jan@kwoh.de>
2021-07-06 22:24:55 +0200

    - fast import with bulk_save_objects - reading json in chunks with ipython - import new dataset, then deletes the old one

--- hubgrep/cli_blueprint/import_data.py
+++ hubgrep/cli_blueprint/import_data.py
@@ -1,3 +1,4 @@
+import ijson
 import gzip
 import tempfile
 import click
@@ -15,7 +16,7 @@ import requests
 import json
 import datetime
 import logging
-
+from sqlalchemy import and_
 
 logger = logging.getLogger(__name__)
 
@@ -24,49 +25,73 @@ class DateTimeDecoder(json.JSONDecoder):
     """
     json encoder that can dump datetimes
     """
+
     pass
 
 
 def add_hoster(hoster_dict: dict):
-    api_url = hoster_dict['api_url']
+    api_url = hoster_dict["api_url"]
     hosting_service = HostingService.query.filter_by(api_url=api_url).first()
     if not hosting_service:
         hosting_service = HostingService()
 
     hosting_service.label = api_url
-    hosting_service.api_url = hoster_dict['api_url']
-    hosting_service.landingpage_url = hoster_dict['landingpage_url']
-    hosting_service.type = hoster_dict['type']
+    hosting_service.api_url = hoster_dict["api_url"]
+    hosting_service.landingpage_url = hoster_dict["landingpage_url"]
+    hosting_service.type = hoster_dict["type"]
     hosting_service.has_local_index = True
     db.session.add(hosting_service)
     db.session.commit()
     return hosting_service
 
-def fetch_hoster_repos(export_url, hoster):
-    logger.info(f'fetching {export_url}')
-    with tempfile.TemporaryFile(mode='w+b') as f:
 
+def fetch_hoster_repos(export_url, hoster, import_timestamp):
+    logger.info(f"fetching {export_url}, {hoster}, {import_timestamp}")
+    with tempfile.TemporaryFile(mode="w+b") as f:
         r = requests.get(export_url, stream=True)
         for chunk in r.iter_content(chunk_size=1024):
-            if chunk: # filter out keep-alive new chunks
+            if chunk:  # filter out keep-alive new chunks
                 f.write(chunk)
 
+        logger.info(f"done fetching! ({f.tell()})")
         f.seek(0)
-        gz_file = gzip.GzipFile(fileobj=f)
-        repos = json.load(gz_file)
-        chunk_size = 0
-        for repo in repos:
-            chunk_size += 1
-            if repo:
-                r = Repository.from_dict(repo, hoster)
-                db.session.add(r)
-                if chunk_size >= 1000:
-                    logger.info('commiting chunk...')
-                    db.session.commit()
-                    chunk_size = 0
-        logger.info('commiting chunk...')
-        db.session.commit()
 
+        # clean up: if we import the same export twice, we need to get rid of the old one first
+        Repository.__table__.delete().where(
+            and_(
+                Repository.hosting_service_id == hoster.id,
+                Repository.import_timestamp == import_timestamp,
+            )
+        )
+
+        gz_file = gzip.GzipFile(fileobj=f)
+
+        chunk_size = 0
+        repos_to_add = []
+
+        for repo in ijson.items(gz_file, 'item'):
+            chunk_size += 1
+            r = Repository.from_dict(repo, hoster, import_timestamp)
+            if r:
+                repos_to_add.append(r)
+            if chunk_size >= 10000:
+                logger.info("commiting chunk...")
+                before = time.time()
+                db.session.bulk_save_objects(repos_to_add)
+                db.session.commit()
+                logger.info(f'took {time.time() - before}')
+                chunk_size = 0
+                repos_to_add = []
+        
+        logger.info("commiting last chunk...")
+        db.session.bulk_save_objects(repos_to_add)
+        db.session.commit()
+        Repository.__table__.delete().where(
+            and_(
+                Repository.hosting_service_id == hoster.id,
+                Repository.import_timestamp != import_timestamp,
+            )
+        )
         """
         # reading in chunks
         f.seek(0)
@@ -78,24 +103,22 @@ def fetch_hoster_repos(export_url, hoster):
             print(prefix, key, value)
         """
 
+
 @cli_bp.cli.command(help="import repo data")
 def import_repos():
     db.engine.dialect.psycopg2_batch_mode = True
-    indexer_url = current_app.config['INDEXER']
-    hosters_url = urllib.parse.urljoin(indexer_url, 'api/v1/export_hosters')
+    indexer_url = current_app.config["INDEXER"]
+    hosters_url = urllib.parse.urljoin(indexer_url, "api/v1/export_hosters")
     response = requests.get(hosters_url)
     print(response.text)
     hosters = response.json()
 
     for hoster_dict in hosters:
         hoster = add_hoster(hoster_dict)
-        exports = hoster_dict.get('exports')
+        exports = hoster_dict.get("exports")
         if exports:
-            export_url = exports[0]['url']
-            fetch_hoster_repos(export_url, hoster)
-
-
-
-
-
-
+            export_url = exports[0]["url"]
+            import_timestamp = datetime.datetime.fromisoformat(
+                exports[0]["created_at"]
+            ).timestamp()
+            fetch_hoster_repos(export_url, hoster, import_timestamp)
--- hubgrep/models/repository.py
+++ hubgrep/models/repository.py
@@ -2,7 +2,6 @@ import logging
 import urllib.parse
 from hubgrep import db
 from sqlalchemy import Index
-
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -15,13 +14,12 @@ class Repository(db.Model):
     __tablename__ = "repositories"
     __table_args__ = (Index("repo_index", "foreign_id", "hosting_service_id"),)
 
-    id = db.Column(db.Integer, primary_key=True)
-
     # id on the hosting_service
-    foreign_id = db.Column(db.Integer)
+    foreign_id = db.Column(db.Integer, primary_key=True)
+    import_timestamp = db.Column(db.Float, primary_key=True)
 
     hosting_service_id = db.Column(
-        db.Integer, db.ForeignKey("hosting_service.id"), nullable=False
+        db.Integer, db.ForeignKey("hosting_service.id"), nullable=False, primary_key=True
     )
     hosting_service = db.relationship(
         "HostingService", backref=db.backref("repos", lazy=True)
@@ -49,22 +47,79 @@ class Repository(db.Model):
     license_name = db.Column(db.String(500), nullable=True)
 
     @classmethod
-    def from_dict(cls, d, hosting_service: 'HostingService'):
+    def from_dict(cls, d, hosting_service: 'HostingService', import_timestamp):
         if hosting_service.type == "gitea":
-            return cls.from_gitea_dict(d, hosting_service)
+            return cls.from_gitea_dict(d, hosting_service, import_timestamp)
         elif hosting_service.type == "github":
-            return cls.from_github_dict(d, hosting_service)
+            return cls.from_github_dict(d, hosting_service, import_timestamp)
+    @classmethod
+    def to_unified_dict(cls, d, hosting_service, import_timestamp):
+        if hosting_service.type == "gitea":
+            return cls.to_unified_from_gitea_dict(d, hosting_service, import_timestamp)
+        elif hosting_service.type == "github":
+            return cls.to_unified_from_github_dict(d, hosting_service, import_timestamp)
 
     @classmethod
-    def from_gitea_dict(cls, d, hosting_service):
+    def to_unified_from_gitea_dict(cls, d, hosting_service, import_timestamp):
         foreign_id = d["gitea_id"]
-        r = cls.query.filter_by(
-            hosting_service_id=hosting_service.id, foreign_id=foreign_id
-        ).first()
-        if not r:
-            r = cls()
-            r.hosting_service_id = hosting_service.id
-            r.foreign_id = foreign_id
+        r = {}
+        r['hosting_service_id'] = hosting_service.id
+        r['foreign_id'] = foreign_id
+        r['import_timestamp'] = import_timestamp
+        r['name'] = d["name"]
+        r['namespace'] = d["owner_username"]
+        r['description'] = d["description"]
+        r['subscribers_count'] = d['watchers_count']
+        # r['is_empty'] = d["empty"]
+        r['is_fork'] = d["fork"]
+        # r['is_mirror'] = d["mirror"] = self.mirror
+        r['size'] = d["size"]
+        r['homepage'] = d["website"]
+        path_to_user = urllib.parse.urljoin(hosting_service.landingpage_url, r.namespace + "/")
+        r['html_url'] = urllib.parse.urljoin(path_to_user, r.name)
+        r['stars_count'] = d["stars_count"]
+        r['forks_count'] = d["forks_count"]
+        r['watchers_count'] = d["watchers_count"]
+        r['open_issues_count'] = d["open_issues_count"]
+        r['default_branch'] = d["default_branch"]
+        r['created_at'] = d["created_at"]
+        r['updated_at'] = d["updated_at"]
+        return r
+
+    @classmethod
+    def to_unified_from_github_dict(cls, d, hosting_service, import_timestamp):
+        foreign_id = d["github_id"]
+        r = {}
+        r['hosting_service_id'] = hosting_service.id
+        r['foreign_id'] = foreign_id
+        r['import_timestamp'] = import_timestamp
+        r['name'] = d["name"]
+        r['namespace'] = d["owner_login"]
+        r['description'] = d["description"]
+        r['is_empty'] = d["is_empty"]
+        r['is_fork'] = d["is_fork"]
+        # r['is_mirror'] = d["mirror"] = self.mirror
+        r['size'] = d["disk_usage"]
+        r['homepage'] = d["homepage_url"]
+        r['html_url'] = d['url']
+        r['stars_count'] = d["stargazer_count"]
+        r['forks_count'] = d["fork_count"]
+        #r['watchers_count'] = d["watchers_count"]
+        #r['open_issues_count'] = d["open_issues_count"]
+        #r['default_branch'] = d["default_branch"]
+        r['created_at'] = d["created_at"]
+        r['updated_at'] = d["updated_at"]
+        r['pushed_at'] = d['pushed_at']
+        r['license_name'] = d['license_name']
+        return r
+
+    @classmethod
+    def from_gitea_dict(cls, d, hosting_service, import_timestamp):
+        foreign_id = d["gitea_id"]
+        r = cls()
+        r.hosting_service_id = hosting_service.id
+        r.foreign_id = foreign_id
+        r.import_timestamp = import_timestamp
         r.name = d["name"]
         r.namespace = d["owner_username"]
         r.description = d["description"]
@@ -86,15 +141,12 @@ class Repository(db.Model):
         return r
 
     @classmethod
-    def from_github_dict(cls, d, hosting_service):
+    def from_github_dict(cls, d, hosting_service, import_timestamp):
         foreign_id = d["github_id"]
-        r = cls.query.filter_by(
-            hosting_service_id=hosting_service.id, foreign_id=foreign_id
-        ).first()
-        if not r:
-            r = cls()
-            r.hosting_service_id = hosting_service.id
-            r.foreign_id = foreign_id
+        r = cls()
+        r.hosting_service_id = hosting_service.id
+        r.foreign_id = foreign_id
+        r.import_timestamp = import_timestamp
         r.name = d["name"]
         r.namespace = d["owner_login"]
         r.description = d["description"]
