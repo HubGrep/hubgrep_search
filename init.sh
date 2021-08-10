26a0ccb48ed6fc26d3b836e93e267e977c97b437
jan <jan@kwoh.de>
2021-08-11 00:32:47 +0200

    fixes in compose, update env.dist
    ... define dockerfile in .env

--- init.sh
+++ init.sh
@@ -7,27 +7,29 @@
 set -e
 set -x
 
+source .env
+
 docker_compose=docker-compose  #/usr/local/bin/docker-compose
 temp_table_name=new_repositories
 
 # sphinx doesnt like being started without having an index,
 # so we skip it here and only start postgres and hubgrep
-$docker_compose up -d postgres service
+$docker_compose -f ${DOCKERFILE} up -d postgres service
 
 # initialize postgres db
-$docker_compose exec service /bin/bash -ic "flask db upgrade"
+$docker_compose -f ${DOCKERFILE} exec service /bin/bash -ic "flask db upgrade"
 
 # import new data from indexer
-$docker_compose exec service /bin/bash -ic "flask cli import-repos ${temp_table_name}"
+$docker_compose -f ${DOCKERFILE} exec service /bin/bash -ic "flask cli import-repos ${temp_table_name}"
 
 # create the first search index and start sphinx afterwards
-$docker_compose run sphinx cat /opt/sphinx/conf/sphinx.conf
-$docker_compose run sphinx indexer --all --config /opt/sphinx/conf/sphinx.conf
-$docker_compose up -d sphinx
+$docker_compose -f ${DOCKERFILE} run sphinx cat /opt/sphinx/conf/sphinx.conf
+$docker_compose -f ${DOCKERFILE} run sphinx indexer --all --config /opt/sphinx/conf/sphinx.conf
+$docker_compose -f ${DOCKERFILE} up -d sphinx
 
 # rotate db tables
-$docker_compose exec service /bin/bash -ic "flask cli rotate-repositories-table ${temp_table_name}"
+$docker_compose -f ${DOCKERFILE} exec service /bin/bash -ic "flask cli rotate-repositories-table ${temp_table_name}"
 
 # start everything!
-$docker_compose up -d
+$docker_compose -f ${DOCKERFILE} up -d
 
--- update.sh
+++ update.sh
@@ -7,19 +7,21 @@
 set -e
 set -x
 
+source .env
+
 docker_compose=docker-compose  #/usr/local/bin/docker-compose
 temp_table_name=new_repositories
 
 
-$docker_compose up -d sphinx postgres service
+$docker_compose -f ${DOCKERFILE} up -d sphinx postgres service
 
 # import new data from indexer
-$docker_compose exec service /bin/bash -ic "flask cli import-repos ${temp_table_name}"
+$docker_compose -f ${DOCKERFILE} exec service /bin/bash -ic "flask cli import-repos ${temp_table_name}"
 
 # create new search index, rotate search index
 # (todo: table name is hardcoded in sphinx.conf for now...)
-$docker_compose exec sphinx cat /opt/sphinx/conf/sphinx.conf
+$docker_compose -f ${DOCKERFILE} exec sphinx cat /opt/sphinx/conf/sphinx.conf
 # todo: if data is empty: docker-compose run sphinx indexer --all --config /opt/sphinx/conf/sphinx.conf --rotate
-$docker_compose exec sphinx indexer --all --config /opt/sphinx/conf/sphinx.conf --rotate
+$docker_compose -f ${DOCKERFILE} exec sphinx indexer --all --config /opt/sphinx/conf/sphinx.conf --rotate
 
-$docker_compose exec service /bin/bash -ic "flask cli rotate-repositories-table ${temp_table_name}"
+$docker_compose -f ${DOCKERFILE} exec service /bin/bash -ic "flask cli rotate-repositories-table ${temp_table_name}"
