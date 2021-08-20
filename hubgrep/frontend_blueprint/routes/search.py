7eff89824296a4b1596ff66b6cee25d485c0963c
jan <jan@kwoh.de>
2021-08-20 20:09:55 +0200

    only fetch the current page from postgres

--- hubgrep/frontend_blueprint/routes/search.py
+++ hubgrep/frontend_blueprint/routes/search.py
@@ -8,7 +8,7 @@ from flask import request
 
 from hubgrep.constants import PARAM_OFFSET, PARAM_PER_PAGE, FORM_ARGS
 from hubgrep.lib.pagination import get_page_links
-from hubgrep.lib.manticore import ManticoreSearch
+from hubgrep.lib.manticore import ManticoreSearch, SearchMeta
 from hubgrep.lib.manticore import UserError
 from hubgrep.lib.search_form import SearchForm
 from hubgrep.frontend_blueprint import frontend
@@ -40,10 +40,13 @@ def search():
     )
     user_errors = []
     pagination_links = []
+    meta = SearchMeta([])
     if form.search_phrase:
         try:
-            results, meta = ManticoreSearch.search(
+            results_page, meta = ManticoreSearch.search(
                 form.search_phrase,
+                results_offset=results_offset,
+                results_per_page=results_per_page,
                 exclude_hosting_service_ids=form.get_excluded_hosting_service_ids(),
                 exclude_forks=form.exclude_forks,
                 exclude_archived=form.exclude_archived,
@@ -52,17 +55,16 @@ def search():
                 created_after=form.created_after_dt,
                 created_before=form.created_before_dt,
                 updated_after=form.updated_after_dt,
-                pushed_after=form.pushed_after_dt
+                pushed_after=form.pushed_after_dt,
             )
         except UserError as e:
-            results = []
+            results_page = []
             user_errors.append(e)
 
         if meta.warning:
             user_errors.append(meta.warning)
 
-        results_paginated = results[results_offset:(results_offset + results_per_page)]
-        pagination_links = get_page_links(request.full_path, results_offset, results_per_page, len(results))
+        pagination_links = get_page_links(request.full_path, results_offset, results_per_page, meta.total)
 
     template_path = (
         "search/search_list.html" if form.search_phrase else "search/landing_page.html"
@@ -74,7 +76,7 @@ def search():
     return render_template(
         template_path,
         form=form,
-        search_results=results_paginated,
+        search_results=results_page,
         search_meta=meta,
         total_time=total_time,
         pagination_links=pagination_links,
